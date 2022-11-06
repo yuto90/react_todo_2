be1d2e4be1f3ec9766ed3383bd3c7e0ca9b71fb3
yuto90 <solidusgako4087@gmail.com>
2022-11-06 20:07:25 +0900

    firestoreと接続

--- reactApp/react-sample/src/components/templates/TplMain.tsx
+++ reactApp/react-sample/src/components/templates/TplMain.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../styles.css";
 import { OrgInputTodo } from "../organisms/OrgInputTodo";
 import { OrgInCompleteTodos } from "../organisms/OrgInCompleteTodo";
 import { OrgCompleteArea } from "../organisms/OrgCompleteArea";
+import  db  from '../../firebase';
+import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
 
 export const TplMain = () => {
   const [todoText, setTodoText] = useState("");
   const [inCompleteTodos, setInCompleteTodos] = useState<string[]>([]);
   const [completeTodos, setCompleteTodos] = useState<string[]>([]);
+  const [posts, setPosts] = useState<any>([]);
+
+  // * firestoreからtodoを取得する
+  useEffect(() => { 
+    const postData = collection(db, 'posts');
+    getDocs(postData).then((snapshot: QuerySnapshot) => {
+      setPosts(snapshot.docs.map((doc) => ({ ...doc.data() })));
+    })
+  }, [])
 
   const onChangeTodoText = (event:any) => setTodoText(event.target.value);
 
